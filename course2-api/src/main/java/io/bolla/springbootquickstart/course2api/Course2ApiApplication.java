618e3541045cef451bfecae7d652ad4c299d79dd
Venkateshyz <36966901+Venkateshyz@users.noreply.github.com>
2018-06-03 14:38:47 -0500

    uploading course2 api into toupload

---
+++ course2-api/src/main/java/io/bolla/springbootquickstart/course2api/Course2ApiApplication.java
@@ -0,0 +1,12 @@
+package io.bolla.springbootquickstart.course2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2ApiApplication.class, args);
+	}
+}
